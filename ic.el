29e0212fe95a111196076d60dd2b9c234d5f0f2d
Jen-Chieh Shen <jcs090218@gmail.com>
2024-01-01 01:42:17 -0800

    chore: Update copyright year

--- ic.el
+++ ic.el
@@ -1,6 +1,6 @@
 ;;; ic.el --- Pretty print to debug  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2023  Shen, Jen-Chieh
+;; Copyright (C) 2023-2024  Shen, Jen-Chieh
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
